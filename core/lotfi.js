19f3fb6657986bddec9f361698a0590e91e600e1
Yudho Ahmad Diponegoro <yudh0002@e.ntu.edu.sg>
2012-11-24 19:30:00 -0800

    input feature

--- core/lotfi.js
+++ core/lotfi.js
@@ -1,14 +1,22 @@
 function lotfi(){	
 	function newset(setobject){			
 			this.name=setobject.name;
-			this.type=(setobject.type=='continuous'?setobject.type:'discrete');
+			this.type=(setobject.type=='continuous'?setobject.type:'discrete');			
+			this.number_of_var=setobject.number_of_var;
+			for(var i=0;i<this.number_of_var;i++){
+				eval('var temp=setobject.var'+i+'.name;');
+				eval('this.'+temp+'=setobject.var'+i+';');				
+			}
+			
 			//fuzzy set name validation, cannot be same as lotfi functions.
 			if(this.name=='lotfi'||this.name=='newset'){
 				logerror({type:1,variable_name:this.name});
 				return
 			}
-			//fuzzy set name validation till here
-			eval('lotfi.'+this.name+'=this;'); //declare the fuzzy variable
+			//fuzzy set name validation till here			
+			eval('lotfi.'+this.name+'=this;'); //declare the fuzzy variable	
+			
+			
 	}
 	function destroyset(setname){
 		eval('if(lotfi.'+setname+'==undefined){logerror({type:2,variable_name:\''+setname+'\'});}else{lotfi.'+setname+'=null;}');
@@ -35,5 +43,6 @@ function lotfi(){
 
 //unit tests
 var lotfi=new lotfi();
-lotfi.newset({name:'hahaha',type:'discrete'});
+lotfi.newset({name:'temperature',type:'discrete',number_of_var:3,var0:{name:'low'},var1:{name:'medium'},var2:{name:'high'}});
+alert(lotfi.temperature.low);
 //unit tests till here
